5c42a073e7245836b6a668c97941a67f3458c691
Harvey Lallave <harvey_lallave@dlsu.edu.ph>
2017-07-27 19:49:24 +0800

    Temporary load more (Ajax)

--- script.js
+++ script.js
@@ -1,6 +1,146 @@
 
+    var loaded = 0;
+    var loadA = 0;
+    var root = 'http://jsonplaceholder.typicode.com';
+    var max = function () {
+        var tmp = null;
+        $.ajax({
+            'async': false,
+            url: root + '/photos/',
+            method: 'GET',
+            success: (function(data) {
+                tmp = data.length;
+            }),
+        });
+        return tmp;
+    }();
+
+    function loadPhotos(){
+
+        var stringDiv = document.createElement("div");
+
+        //add classes
+        $(stringDiv).addClass("string");
+
+        for(i = 1; i <= 5 && max && i < max; i++){
+            (function(i, loadA, stringDiv) {
+                $.ajax({
+                    url: root + '/photos/',
+                    method: 'GET',
+                    error: function()
+                    {
+                        //file does not exists
+                        console.log("File does not exist");
+                    },
+                    success: function(data)
+                    {
+                        generateAlbumInfo(data[max - loadA - i], stringDiv);
+                    }
+                });
+            })(i, loadA, stringDiv);
+        }
+        loadA += 5;
+        return stringDiv;
+    }
+
+    function generateAlbumInfo(data, stringDiv) {
+        (function(data, stringDiv) {
+            $.ajax({
+                url: root + '/albums/' + data.albumId,
+                method: 'GET',
+                error: function()
+                {
+                    //file does not exists
+                    console.log("File does not exist");
+                },
+                success: function(album)
+                {
+                    generateUserInfo(data, album, stringDiv);
+                }
+            });
+        })(data, stringDiv);
+    }
+
+    function generateUserInfo(data, album, stringDiv) {
+        (function(data, album, stringDiv) {
+            $.ajax({
+                url: root + '/users/' + album.userId,
+                method: 'GET',
+                error: function()
+                {
+                    //file does not exists
+                    console.log("File does not exist");
+                },
+                success: function(user)
+                {
+                    postPhoto(data, album, user, stringDiv);
+                }
+            });
+        })(data, album, stringDiv);
+    }
+
+    function postPhoto(data, album, user, stringDiv){
+        //create elements
+        var pin          = document.createElement("div");
+        var groove       = document.createElement("div");
+        var metal        = document.createElement("div");
+        var polaroid     = document.createElement("div");
+        var thumbnail    = document.createElement("div");
+
+        var modal = document.createElement("div");
+        var close = document.createElement("span");
+        var photo = document.createElement("div");
+        var caption = document.createElement("div");
+
+        //add classes
+        $(pin).addClass("pin");
+        $(groove).addClass("groove");
+        $(metal).addClass("metal");
+        $(polaroid).addClass("polaroid");
+        $(thumbnail).addClass("thumbnail");
+
+        $(modal).addClass("modal");
+        $(close).addClass("close");
+        $(photo).addClass("photo");
+        $(caption).addClass("caption");
+
+        $(close).text("x");
+        $(caption).append("Title: " + data.title +
+                        "<br>Album: " + '<a id="album" albumId="' +
+                        album.id + '" userId = "' + user.username +
+                       '">' + album.title + '</a>' +
+                       "<br>By: " + '<a id = "username" userId = "' +
+                        user.id + '">' + user.username + '</a>');
+        //assemble
+        $(photo).prepend('<img id="theImg" src="' + data.url + '.png" />');
+        $(modal).append(close);
+        $(modal).append(photo);
+        $(modal).append(caption);
+
+        $(thumbnail).prepend('<img id="theImg" src="' + data.thumbnailUrl + '.png" />');
+        $(pin).append(groove);
+        $(groove).append(metal);
+        $(polaroid).append(thumbnail);
+
+        $(stringDiv).append(modal);
+        $(stringDiv).append(pin);
+        $(stringDiv).append(polaroid);
+
+        $(polaroid).click(function(){
+            modal.style.display = "flex";
+        });
+
+        $(close).click(function(){
+            modal.style.display = "none";
+        });
+    }
 
 $(document).ready(function(){
+
+    for(i = 0; i < 3; i++){     // 3 strings * 5 photos each
+        var stringDiv = loadPhotos();
+        $(".feed").append(stringDiv);
+    }
 
 //    loadPosts();
 //    loadAlbums();
@@ -113,4 +253,19 @@ $(document).ready(function(){
     });
 
 
+     $(window).scroll(function() {
+        var div = $(this);
+        var scroll = 0;
+        setTimeout(function() {
+            scroll = div.scrollTop();
+        }, 200);
+        if (Math.round($(this).scrollTop()) == $(".feed")[0].scrollHeight - window.innerHeight &&
+            document.getElementsByClassName("close")[0].style.display == "none") { //scrollTop is 0 based
+            console.log("Load more!");
+            for(i = 0; i < 3; i++){     // 3 strings * 5 photos each
+                var stringDiv = loadPhotos();
+                $(".feed").append(stringDiv);
+            }
+        }
+    });
 });
