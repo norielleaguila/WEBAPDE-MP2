23557524659e70b163f6a7fb9ffd1b7896fee81a
Harvey Lallave <harvey_lallave@dlsu.edu.ph>
2017-08-01 11:48:28 +0800

    asd

--- script.js
+++ script.js
@@ -244,13 +244,13 @@ function postPhoto(data, album, user, stringDiv){
     $(caption).append(ct);
 
     //assemble
-    $(photo).prepend('<img id="theImg" src="' + "https" + data.url.substring(4, data.url.length) + '.png" />');
+    $(photo).prepend('<img id="theImg" src="' + "https" + data.thumbnailUrl.substring(4, data.thumbnailUrl.length) + '.png" />');
     $(photo).append(tag);
     $(modal).append(close);
     $(modal).append(photo);
     $(modal).append(caption);
 
-    $(thumbnail).prepend('<img id="theImg" src="' + "https" + data.url.substring(4, data.url.length)  + '.png" />');
+    $(thumbnail).prepend('<img id="theImg" src="' + "https" + data.thumbnailUrl.substring(4, data.thumbnailUrl.length)  + '.png" />');
     $(pin).append(groove);
     $(groove).append(metal);
     $(polaroid).append(thumbnail);
