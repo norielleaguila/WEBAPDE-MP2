d3425d7ac05ac04875c25de73dfd11a97896a2dc
Harvey Lallave <harvey_lallave@dlsu.edu.ph>
2017-08-01 18:00:55 +0800

    sdasd

--- script.js
+++ script.js
@@ -270,7 +270,8 @@ function postPhoto(data, album, user, stringDiv){
        (user.id == 1 || data.id == 4996 || data.id == 4992 || data.id == 4991) &&
        loggedInUser != "" && loggedInUser.toUpperCase() == user.username.toUpperCase()){
         $(polaroid).append("<p>Shared With You</p>");
-    }else{
+    }else if(document.getElementById("checkBoxPane") == null || (document.getElementById("checkBoxPane") != null && !document.getElementById("checkBoxPane").checked)){
+
         $(polaroid).append("<p>Public</p>");
     }
 
